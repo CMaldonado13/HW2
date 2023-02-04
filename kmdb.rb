78daeceec7d223a4fe1488626e26c10870a7761b
CMaldonado13 <christian.maldonado1@kellogg.northwestern.edu>
2023-02-04 19:07:09 +0000

    test

--- kmdb.rb
+++ kmdb.rb
@@ -69,6 +69,7 @@
 
 # Delete existing data, so you'll start fresh each time this script is run.
 # Use `Model.destroy_all` code.
+Model.destroy_all
 # TODO!
 
 # Generate models and tables, according to the domain model.
